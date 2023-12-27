7354aa4f8fee0863eed9a695481e65e34843e6a7
Chris Hennick <hennickc@amazon.com>
2023-12-26 20:15:34 -0800

    Simplify: don't need to rotate by a prime

--- src/core.rs
+++ src/core.rs
@@ -14,23 +14,14 @@ pub const STRIPE_MASKS: [Word; 6] = [
     0xffffffff00000000
 ];
 
-// PRIME_ROTATION_AMOUNTS is padded to speed up copying; NUM_PRIMES is its real length.
-pub const NUM_PRIMES: usize = 11;
-pub const PRIME_ROTATION_AMOUNTS: [u8; NUM_PRIMES] = [
-    2, 3, 5, 7,
-    11, 13, 17, 19,
-    23, 29, 31];
-
 #[inline]
 pub fn shift_stripe(input: Word, mut permutor: Word) -> Word {
     let mut out = input;
     //permutor = permutor.rotate_right(round);
     for i in 0..8 {
-        let rotation_selector = (permutor as usize).wrapping_add(i) % NUM_PRIMES;
         out ^= STRIPE_MASKS[(permutor % 6) as usize];
-        out ^= out.rotate_right(PRIME_ROTATION_AMOUNTS[rotation_selector] as u32)
-            .wrapping_add(META_PERMUTOR);
-        let swap_selector = ((permutor / 6) % 6) as usize;
+        out ^= out.wrapping_add(META_PERMUTOR).rotate_right((3 + 2*i) as u32);
+        let swap_selector = ((permutor >> 3) % 6) as usize;
         let swap_mask = STRIPE_MASKS[swap_selector];
         out = (out & swap_mask).shr(1.shl(swap_selector)) | (out & !swap_mask).shl(1.shl(swap_selector));
         permutor >>= 8;
