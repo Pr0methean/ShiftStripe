20cdf75ab97ba1cd29d3e6136ab467eed930615c
Chris Hennick <hennickc@amazon.com>
2023-12-31 15:53:56 -0800

    Bug fix for low diffusion with 4 rounds?

--- src/core.rs
+++ src/core.rs
@@ -34,7 +34,7 @@ pub fn shift_stripe(input: Word, mut permutor: Word) -> Word {
         let rotation_selector = (permutor & 3) as u32;
         permutor >>= 2;
         out ^= (out ^ stripe_mask)
-            .wrapping_add(META_PERMUTOR).rotate_right(1 + 2 * rotation_selector);
+            .wrapping_add(META_PERMUTOR).rotate_right(3 + 2 * rotation_selector);
         let swap_mask = STRIPE_MASKS[swap_selector as usize];
         out = (out & swap_mask).shr(1.shl(swap_selector)) | (out & !swap_mask).shl(1.shl(swap_selector));
     });
