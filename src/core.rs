c8919b6cbf00c9460de4a333dba4f9889b0d76a1
Chris Hennick <hennickc@amazon.com>
2024-01-12 21:56:17 -0800

    Bug fix: use split_at_mut to obtain swappable borrows in shuffled_mask_indices

--- src/core.rs
+++ src/core.rs
@@ -37,8 +37,9 @@ fn shuffled_mask_indices(n : &mut Vector) -> [VectorUsize; STRIPE_MASKS.len()] {
         let modulus = Vector::splat(i as Word + 1);
         let j = *n % modulus;
         *n /= &modulus;
+        let (left_mut, right_mut) = indices.split_at_mut(i + 1);
         for lane in 0..VECTOR_SIZE {
-            swap(&mut indices[i][lane], &mut indices[j[lane] as usize][lane]);
+            swap(&mut left_mut[i][lane], right_mut[j[lane]][lane]);
         }
     }
     array![i => VectorUsize::from_array(indices[i]); STRIPE_MASKS.len()]
